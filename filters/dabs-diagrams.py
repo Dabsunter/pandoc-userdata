278fa1febff5e4ff41b21822a7b3f312b037fccf
David Nicolazo <git@dabsunter.fr>
2022-03-31 20:20:55 +0200

    update dabs-diagrams

--- filters/dabs-diagrams.py
+++ filters/dabs-diagrams.py
@@ -1,31 +1,44 @@
 #!/usr/bin/env python3
 
 from panflute import *
-import pandocfilters as pf
+from pandocfilters import get_filename4code, get_extension
+import sys
+import os
+
+def eprint(*args, **kwargs):
+    print(*args, file=sys.stderr, **kwargs)
 
 FILTER_NAME = "dabs-diagrams"
 
 def render_mermaid(elem, doc):
-    if type(elem) == pf.CodeBlock:
-        if 'mermaid' in elem.classes:
-            filename = pf.get_filename4code(
-                FILTER_NAME,
-                elem.text,
-                'pdf' if doc.format == 'latex' else 'svg'
+    if type(elem) == CodeBlock and 'mermaid' in elem.classes:
+        outputname = get_filename4code(
+            FILTER_NAME,
+            elem.text,
+            get_extension(doc.format, 'png', latex='pdf', html='svg')
+        )
+
+        if not os.path.isfile(outputname):
+            shell(
+                [
+                    "mmdc",
+                    # "--input", inputname,
+                    "--output", outputname,
+                    "--pupeteerConfig", "~/pupeteer-config.json"
+                    "--pdfFit"
+                ],
+                msg=elem.text.encode('utf8')
             )
 
-            shell(f"""mmdc
-                --output {filename}
-                --pdfFit
-                --puppeteerConfigFile ~/puppeteer-config.json""",
-                msg=elem.text)
+            # eprint("response", res)
             
-            return Image(
-                url=filename,
-                title=elem.attribute['caption'],
-                classes=elem.classes,
-                attributes=elem.attributes
-            )
+        return Para(Image(
+            *convert_text(elem.attributes['caption'])[0].content if 'caption' in elem.attributes else [],
+            url=outputname,
+            title='fig:' if 'caption' in elem.attributes else '',
+            classes=elem.classes,
+            attributes=elem.attributes
+        ))
             
 
 
@@ -34,3 +47,4 @@ def main(doc=None):
 
 if __name__ == "__main__":
     main()
+
